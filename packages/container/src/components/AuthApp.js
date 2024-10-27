6cf424176be6c8fb6721aba91abaec9bddc8252f
Marko Perendic <perendicsmarko@gmail.com>
2024-10-27 14:28:16 +0100

    added Auth to Container

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,26 @@
+import { mount } from "auth/AuthApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (ref.current) {
+      const { onParentNavigate } = mount(ref.current, {
+        onNavigate: ({ pathname: nextPathname }) => {
+          const { pathname } = history.location;
+
+          if (pathname !== nextPathname) {
+            history.push(nextPathname);
+          }
+        },
+      });
+
+      history.listen(onParentNavigate);
+    }
+  }, []);
+
+  return <div ref={ref} />;
+};
